4669e9a3bfa119af8215a039b342225187dd2493
impks27 <37983994+impks27@users.noreply.github.com>
2020-08-04 18:23:39 +0530

    Update Pipeline.groovy

--- Pipeline.groovy
+++ Pipeline.groovy
@@ -10,7 +10,7 @@ def buildAndPushImage() {
     def registry = "impks/test-bonjwa"
     def registryCredential = "impks-dockerHub"
     stage('Build and Push Image') {
-        dockerImage = docker.build registry + ":$BUILD_NUMBER"
+        dockerImage = docker.build registry + ":latest" //":$BUILD_NUMBER" Setting latest for demo
         docker.withRegistry('', registryCredential) {
             dockerImage.push()
         }
